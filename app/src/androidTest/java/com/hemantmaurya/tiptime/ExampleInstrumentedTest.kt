a9514b234ec880591a670d4cf6284f2ebe020da8
Hemant Maurya <hemantmaurya344@gmail.com>
2023-03-01 21:12:53 +0530

    Tiptime App

---
+++ app/src/androidTest/java/com/hemantmaurya/tiptime/ExampleInstrumentedTest.kt
@@ -0,0 +1,32 @@
+package com.hemantmaurya.tiptime
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Rule
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.hemantmaurya.tiptime", appContext.packageName)
+    }
+    @Test
+    fun calculate_20_percent_tip() {
+
+    }
+}
